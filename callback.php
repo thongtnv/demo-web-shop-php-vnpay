5a92a4d4ebecd69115c6ea236a9babb4103665ff
ThongNv <nmthdt@gmail.com>
2026-05-06 16:13:56 +0700

    demo vnpay

---
+++ callback.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once 'config.php';
+
+$vnp_HashSecret = getenv('VNPAY_HASH_SECRET');
+
+$vnp_SecureHash = $_GET['vnp_SecureHash'];
+$inputData = array();
+foreach ($_GET as $key => $value) {
+    if (substr($key, 0, 4) == "vnp_") {
+        $inputData[$key] = $value;
+    }
+}
+
+unset($inputData['vnp_SecureHash']);
+ksort($inputData);
+$i = 0;
+$hashData = "";
+foreach ($inputData as $key => $value) {
+    if ($i == 1) {
+        $hashData = $hashData . '&' . urlencode($key) . "=" . urlencode($value);
+    } else {
+        $hashData = $hashData . urlencode($key) . "=" . urlencode($value);
+        $i = 1;
+    }
+}
+
+echo $secureHash = hash_hmac('sha512', $hashData, $vnp_HashSecret);
+?>
